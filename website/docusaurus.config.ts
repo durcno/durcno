39e5f8f8edbe1865f8fb6c7f5b3247652b570378
Al Mahdi <almahdi404@proton.me>
2026-04-19 21:16:20 +0600

    website: add open graph image

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -128,7 +128,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/og-image.png",
     colorMode: {
       respectPrefersColorScheme: true,
     },
